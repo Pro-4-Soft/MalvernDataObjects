0d604f032f5ecc4c383e7c7223bcd4171aeab810
Pro4Soft <rodion.pronin@pro4soft.com>
2021-11-23 10:14:05 -0500

    clean up

---
+++ Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Console.Out.WriteLineAsync("Hello World!");
+        }
+    }
+}
